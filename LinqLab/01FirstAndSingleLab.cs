5d4436340d8be57c142a647b11d96db534fe1852
cheat2001 <chansocheat.sok@techbodia.com>
2024-12-26 14:16:39 +0700

    init

---
+++ LinqLab/01FirstAndSingleLab.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLab
+{
+   public class FirstAndSingleLab
+    {
+        private static IEnumerable<Sample> Source { get; set; }
+
+        public FirstAndSingleLab()
+        {
+            Source = new SampleDate().GetData();
+        }
+
+        public Sample FirstToGetUserNameEqualToDemo()
+        {
+            var result = new Sample();
+            return result;
+        }
+
+        public Sample FirstOrDefaultToGetUserNameEuqalToSkilltree()
+        {
+            var result = new Sample();
+            return result;
+        }
+
+        public Sample FirstToGetUserNameEuqalToSkilltree()
+        {
+            var result = new Sample();
+            return result;
+        }
+
+        public Sample SingleToGetUserNameEuqalToDemo()
+        {
+            var result = new Sample();
+            return result;
+        }
+
+        public Sample SingleToGetUserNameEuqalToBill()
+        {
+            var result = new Sample();
+            return result;
+        }
+        public Sample SingleOrDefaultToGetUserNameEuqalToBill()
+        {
+            var result = new Sample();
+            return result;
+        }
+
+        public Sample SingleOrDefaultToGetUserNameEuqalToSkillTree()
+        {
+            var result = new Sample();
+            return result;
+        }
+    }
+}
